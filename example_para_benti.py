7f827fe6739731a5e23c7d9c4ec5cbcb5005431e
Agustin <ext_agromero@mercadolibre.com>
2021-03-15 19:54:47 -0300

    ayudines

--- example_para_benti.py
+++ example_para_benti.py
@@ -5,8 +5,12 @@ from bs4 import BeautifulSoup
 html = request.get('tu-pagina-web.com').text
 soup = BeautifulSoup(html, "html.parser")
 
-# hacer cosas con la sopa para extraer lo que necesites: ://www.crummy.com/software/BeautifulSoup/bs4/doc/ 
 data = parse_html(soup)
 
 # output data:
 print(data)
+
+def parse_html(soup):
+    # hacer cosas con la sopa para extraer lo que necesites: ://www.crummy.com/software/BeautifulSoup/bs4/doc/ 
+    return data
+
